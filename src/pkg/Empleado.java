917346084ef2f9d8d5ffb3b10b0f088177367081
cristian <cristian@Cristian>
2024-10-02 18:50:50 +0200

    Commit 1. Primera versión de Empleado

---
+++ src/pkg/Empleado.java
@@ -0,0 +1,49 @@
+package pkg;
+
+public class Empleado {
+	
+	public enum TipoEmpleado{Vendedor, Encargado};
+	float salarioBase = 0;
+	float primas = 0;
+	float extras = 0;
+
+	public Empleado() {
+		
+	}
+	
+	public float calculoNominaBruta(TipoEmpleado tipo, float ventasMes, float horasExtra) {
+		
+		//Determinar salarioBase
+		if(tipo==TipoEmpleado.Vendedor) {
+			salarioBase = 2000;
+		} else if(tipo==TipoEmpleado.Encargado) {
+			salarioBase = 2500;
+		}
+		
+		//Determinar primas
+		if(ventasMes>=1500) {
+			primas = 200;
+		} else if (ventasMes>=1000) {
+			primas = 100;
+		}
+		
+		//Determinar extras
+		extras = horasExtra * 30;
+		
+		return salarioBase + primas + extras;
+	}
+	
+	//Calculo de la nomina neta 
+	public float calculoNominaNeta(float nominaBruta) {
+		float retencion = 0;
+		
+		if (nominaBruta>=2500) {
+			retencion = 0.18f;
+		} else if (nominaBruta>=2100) {
+			retencion = 0.15f;
+		}
+		return nominaBruta * (1-retencion);
+	}
+
+}
+
